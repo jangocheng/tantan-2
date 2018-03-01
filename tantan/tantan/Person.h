e96b800e4238667fd083aad74b9334b3f57c15cb
sdl <15559636155@163.com>
2018-03-01 20:52:24 +0800

    创建了Person类

---
+++ tantan/tantan/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  tantan
+//
+//  Created by OS X  on 18/3/1.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ tantan/tantan/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  tantan
+//
+//  Created by OS X  on 18/3/1.
+//
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
